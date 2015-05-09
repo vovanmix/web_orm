7af9d78acb97eaad1abe280581cfcd7046b9f397
vladimir <singingcode@gmail.com>
2015-05-09 14:08:06 -0700

    Initial commit

--- tests/miscTest.php
+++ tests/miscTest.php
@@ -2,26 +2,28 @@
 
 namespace Vovanmix\WebOrm\Tests;
 
+use Vovanmix\WebOrm\ormPDOClass;
+
 class miscTest extends \PHPUnit_Framework_TestCase
 {
 
     public function testCamelize()
     {
-        $ORM = new \Vovanmix\WebOrm\ormPDOClass(['base' => 'test']);
+        $ORM = new ormPDOClass(['base' => 'test']);
         $result = $ORM::camelize('hello_world');
         $this->assertEquals('HelloWorld', $result);
     }
 
     public function testHumanize()
     {
-        $ORM = new \Vovanmix\WebOrm\ormPDOClass(['base' => 'test']);
+        $ORM = new ormPDOClass(['base' => 'test']);
         $result = $ORM::humanize('hello_world');
         $this->assertEquals('Hello World', $result);
     }
 
     public function testUnderscore()
     {
-        $ORM = new \Vovanmix\WebOrm\ormPDOClass(['base' => 'test']);
+        $ORM = new ormPDOClass(['base' => 'test']);
         $result = $ORM::underscore('HelloWorld');
         $this->assertEquals('hello_world', $result);
     }
